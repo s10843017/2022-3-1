5e890e3d23697d249fd4d68e04bfe55aad9010db
s10843017 <100847785+s10843017@users.noreply.github.com>
2022-03-03 20:36:43 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Document</title>
+</head>
+<body class = "bg-success">
+    <h2 class="bg-warning text-white">This text represents a warning.</h2>
+<marquee><p>Inserting an imaage to web page</p></marquee>
+
+        <p class="text-warning">This text represents a warning.</p>
+        <p class="text-info">This text represents some information.</p>
+    <p class="text-info">This text represents some information.</p>
+        <img src="images/123.jpg"class="rounded" alt="Cinque Terre" >
+        <img src="images/1234.jpg" class="rounded-circle" alt="Cinque Terre" >
+        <img src="images/12345.jpg" class="img-thumbnail" alt="Cinque Terre" >
+
+</body>
+</html>
